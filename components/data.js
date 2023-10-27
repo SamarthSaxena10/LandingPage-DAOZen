7cfea42693b32ba508b816e6e99c77be8bafd53e
Samarth Saxena <samkusaxena@gmail.com>
2023-10-28 00:38:52 +0530

    readme updated

--- components/data.js
+++ components/data.js
@@ -11,7 +11,7 @@ import benefitOneImg from "../public/img/Wallet.jpeg";
 import benefitTwoImg from "../public/img/benefit-two.png";
 
 const benefitOne = {
-  title: "Highlight your benefits",
+  title: "DAO Analytics",
   desc: "You can use this space to highlight your first benefit or a feature of your product. It can also contain an image or Illustration like in the example along with some bullet points.",
   image: benefitOneImg,
   bullets: [
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Hero from "../components/hero";
-import SectionTitle from "../components/sectionTitle";
 
 import { benefitOne, benefitTwo } from "../components/data";
 
@@ -14,17 +13,8 @@ const Home = () => {
         <title>DAO Stats</title>
       </Head>
       <Hero />
-
       <Benefits data={benefitOne} />
       <Benefits imgPos="right" data={benefitTwo} />
-      <SectionTitle
-        pretitle="Watch a video"
-        title="Learn how to fullfil your needs"
-      >
-        This section is to highlight a promo or demo video of your product.
-        Analysts says a landing page with video has 3% more conversion rate. So,
-        don&apos;t forget to add one. Just like this.
-      </SectionTitle>
 
       <Footer />
     </>
