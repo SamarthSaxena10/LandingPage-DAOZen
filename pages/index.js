4afd0ecf5bfe4183dabee42b071ab649b7a5e456
Samarth Saxena <samkusaxena@gmail.com>
2023-10-27 19:11:03 +0530

    FAQ resolved

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,9 @@ import Navbar from "../components/navbar";
 import SectionTitle from "../components/sectionTitle";
 
 import { benefitOne, benefitTwo } from "../components/data";
-import Video from "../components/video";
+
 import Benefits from "../components/benefits";
 import Footer from "../components/footer";
-import Testimonials from "../components/testimonials";
-import Faq from "../components/faq";
 import PopupWidget from "../components/popupWidget";
 
 const Home = () => {
@@ -33,20 +31,6 @@ const Home = () => {
         Analysts says a landing page with video has 3% more conversion rate. So,
         don&apos;t forget to add one. Just like this.
       </SectionTitle>
-      <Video />
-      <SectionTitle
-        pretitle="Testimonials"
-        title="Here's what our customers said"
-      >
-        Testimonails is a great way to increase the brand trust and awareness.
-        Use this section to highlight your popular customers.
-      </SectionTitle>
-      <Testimonials />
-      <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
-        Answer your customers possible questions here, it will increase the
-        conversion rate as well as support or chat requests.
-      </SectionTitle>
-      <Faq />
 
       <Footer />
       <PopupWidget />
