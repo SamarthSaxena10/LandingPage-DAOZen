ad8123c962bb6a6d5ae2b5a2497a6dfb02f5ad48
Samarth Saxena <samkusaxena@gmail.com>
2023-10-28 10:36:25 +0530

    removal of video

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from "next/head";
 import Hero from "../components/hero";
 import Navbar from "../components/navbar";
-import SectionTitle from "../components/sectionTitle";
 
 import { benefitOne, benefitTwo } from "../components/data";
-import Video from "../components/video";
+
 import Benefits from "../components/benefits";
 import Footer from "../components/footer";
 
