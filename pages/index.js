5965de8cf1b2b8c5cf3a314ca0163efa91a76c2f
Samarth Saxena <samkusaxena@gmail.com>
2023-10-28 10:41:59 +0530

    addition of dark and light mode

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,6 @@ import { benefitOne, benefitTwo } from "../components/data";
 import Benefits from "../components/benefits";
 import Footer from "../components/footer";
 
-import PopupWidget from "../components/popupWidget";
-
 const Home = () => {
   return (
     <>
@@ -28,7 +26,6 @@ const Home = () => {
       <Benefits imgPos="right" data={benefitTwo} />
 
       <Footer />
-      <PopupWidget />
     </>
   );
 };
