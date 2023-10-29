74d2d74ba058744e482383d67895480fb933eaa9
Samarth Saxena <90990378+SamarthSaxena10@users.noreply.github.com>
2023-10-29 20:59:29 +0530

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <div className="relative">
       <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
-        Copyright © {new Date().getFullYear()}. Made with ♥ by Samarth and
-        Pragayan
+        Copyright © {new Date().getFullYear()}. Made with ♥ by Samarth Saxena and
+        Pragyanaaditya Borah
       </div>
     </div>
   );
